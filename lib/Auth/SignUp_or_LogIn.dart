6d3de1f10fac82c041ac3cec6665e4934c32307e
Samarth-Ad <samarthad950@gmail.com>
2025-01-09 13:14:46 +0530

    09-01-2025->13:15

---
+++ lib/Auth/SignUp_or_LogIn.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SignupOrLogin extends StatefulWidget {
+  const SignupOrLogin({super.key});
+
+  @override
+  State<SignupOrLogin> createState() => _SignupOrLoginState();
+}
+
+class _SignupOrLoginState extends State<SignupOrLogin> {
+  bool showSignUpPage = true;
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
